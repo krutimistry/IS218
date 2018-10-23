4020b12cd6a8fe82edc1a582fffabe61d8ac5f56
Kruti Mistry <40695828+krutimistry@users.noreply.github.com>
2018-10-23 16:17:25 -0400

    Add files via upload

---
+++ homework/PDOpractice.php
@@ -0,0 +1,29 @@
+<?php
+echo "<h1>PDO Practice</h1>";
+$dsn = 'mysql:host=sql2.njit.edu;dbname=km487';
+$username = 'km487';
+$password = 'humidify0';
+try {
+	    $conn = new PDO($dsn, $username, $password);
+	    echo "Connected successfully<br>";
+	    $sql = "SELECT * FROM test WHERE id < 6";
+	    $q = $conn->prepare($sql);
+	    $q->execute();
+	    $results = $q->fetchAll();
+	    if($q->rowCount() > 0){
+	    	echo 'Results: '. $q->rowCount() . '<br>';
+	    	echo "<table border=\"1\"><tr><th>ID</th><th>Email</th></tr>";
+	    	foreach ($results as $row) {
+        		echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td></tr>";
+    		}
+	    }else{
+	    	echo '0 results';
+	    }
+	    $q->closeCursor();
+    }
+catch(PDOException $e)
+    {
+    	echo "Connection failed: " . $e->getMessage();
+    }
+$conn = null;
+?>
