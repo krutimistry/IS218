dc2df6c3774da9ed46482ade3546596d718e2654
Kruti Mistry <40695828+krutimistry@users.noreply.github.com>
2018-11-07 16:07:57 -0500

    Add files via upload

--- project2/questions.php
+++ project2/questions.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
 <html>
    <body>
-     <head>
       <title>Questions</title>
       <fieldset>
-      <legend>Question Form</legend>
-      <form action = "question.php" method = "GET">
-         Name: <input type = "text" name = "name" required><br><br>
+      <div class="questions-box">
+      <h2>Questions Page</h2>
+      <form action = "questions.php" method = "GET">
+         
+         Name: <input type = "text" class="name" name = "name" required><br>
 
          <label for="body">Body</label>
-         <textarea id="subject" name="body" required style="height:100px"></textarea><br><br>
+         <textarea id="subject" name="body" class="name" required style="height:75px"></textarea><br>
 
          <label for="subject">Skills</label>
-         <textarea id="subject" name="skills" required style="height:200px"></textarea><br><br>
+         <textarea id="subject" name="skills" class="name" required style="height:100px"></textarea><br>
 
-      <input type = "submit"/>
+      <button name='name' class="submit" type="submit">Login</button><br><br>
+      </div>
       </form>
      </fieldset>
-    </head>
    </body>
 </html>
 
@@ -58,3 +59,50 @@ if($contact){
     }
   }
 ?>
+
+
+<style>
+
+body {
+    background-color: #5abdd2;
+    font-family: 'Ubuntu', sans-serif;
+}
+
+.questions-box{
+    background-color: #99c0ff;
+    color: black;
+    margin: auto;
+    padding: 20px;
+    width: 300px;
+    border: 10px solid black;
+    font-family: 'Ubuntu', sans-serif;
+}
+
+.name {
+    width: 76%;
+    font-weight: 700;
+    font-size: 14px;
+    letter-spacing: 1px;
+    background: white;
+    padding: 10px 20px;
+    border: none;
+    outline: none;
+    text-align: center;
+    margin-bottom: 27px;
+    font-family: 'Ubuntu', sans-serif;
+}
+
+
+.submit {
+    cursor: pointer;
+    background: #3366cc;
+    color: #fff;
+    border: 0;
+    padding-left: 40px;
+    padding-right: 40px;
+    padding-bottom: 10px;
+    padding-top: 10px;
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 13px;
+    }
+</style>
