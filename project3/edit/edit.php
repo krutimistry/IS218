eedecff9154002aed2014b13527a1e7782cdaf8f
Kruti Mistry <40695828+krutimistry@users.noreply.github.com>
2018-12-01 22:26:01 -0500

    Add files via upload

---
+++ project3/edit/edit.php
@@ -0,0 +1,117 @@
+<?php
+// including the database connection file
+include_once("config.php");
+ 
+if(isset($_POST['update']))
+{    
+    $id = $_POST['id'];
+    
+    $email=$_POST['email'];
+    $title=$_POST['title'];
+    $body=$_POST['body'];    
+    $skills=$_POST['skills'];  
+	
+    // checking empty fields
+    if(empty($email) || empty($title) || empty($body) || empty($skills)) {            
+        if(empty($email)) {
+            echo "<font color='red'>Email field is empty.</font><br/>";
+        }
+        
+        if(empty($title)) {
+            echo "<font color='red'>Title field is empty.</font><br/>";
+        }
+        
+        if(empty($body)) {
+            echo "<font color='red'>Body field is empty.</font><br/>";
+        } 
+		
+		if(empty($skills)) {
+            echo "<font color='red'>Skills field is empty.</font><br/>";
+        }  
+    } else {    
+        //updating the table
+        $result = mysqli_query($mysqli, "UPDATE Q2 SET email='$email',title='$title',body='$body',skills='$skills' WHERE id=$id");
+        
+        //redirectig to the display page. In our case, it is index.php
+        header("Location: index.php");
+    }
+}
+?>
+<?php
+//getting id from url
+$id = $_GET['id'];
+ 
+//selecting data associated with this particular id
+$result = mysqli_query($mysqli, "SELECT * FROM Q2 WHERE id=$id");
+ 
+while($res = mysqli_fetch_array($result))
+{
+    $email = $res['email'];
+    $title = $res['title'];
+    $body = $res['body'];
+	$skills = $res['skills'];
+}
+?>
+<html>
+<head>    
+    <title>Edit Data</title>
+</head>
+ 
+<body background="geometric-blue-background.jpg">
+<div class="edit-box">
+    <a href="index.php" class="home">Return to Questions Page</a>
+    <br/><br/>
+    
+    <form name="form1" method="post" action="edit.php">
+        <table border="0">
+            <tr> 
+                <td>Email</td>
+                <td><input type="text" name="email" value="<?php echo $email;?>"></td>
+            </tr>
+            <tr> 
+                <td>Title</td>
+                <td><input type="text" name="title" value="<?php echo $title;?>"></td>
+            </tr>
+            <tr> 
+                <td>Body</td>
+                <td><input type="text" name="body" value="<?php echo $body?>"></td>
+            </tr>
+			<tr> 
+                <td>Skills</td>
+                <td><input type="text" name="skills" value="<?php echo $skills;?>"></td>
+            </tr>
+            <tr>
+                <td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
+                <td><input type="submit" name="update" value="Update" class="update"></td>
+            </tr>
+        </table>
+    </form>
+	</div>
+</body>
+</html>
+
+<style>
+.edit-box{
+    background-color: #99c0ff;
+    color: black;
+    margin: auto;
+    padding: 20px;
+    width: 20%;
+    border: 10px solid black;
+}
+
+.home {
+    width: auto;
+	padding: 10px;
+    background: #3366cc;
+    color: #fff;
+}
+
+.update {
+    width: auto;
+    background: #3366cc;
+    color: #fff;
+    border: 1px solid black;
+}
+
+</style>
---
+++ project3/edit/index.php
@@ -0,0 +1,84 @@
+<?php
+
+include_once("config.php");
+ 
+$result = mysqli_query($mysqli, "SELECT * FROM Q2"); 
+?>
+ 
+<html>
+<head>    
+    <title>Homepage</title>
+</head>
+ 
+<body background="geometric-blue-background.jpg">
+<div class="question-box">
+<form action="login.php">
+	<input type="submit" class="button" value="Return to Login Page" />
+</form><br>
+<hr>
+<h1><center>Welcome!<br>Questions Page</center></h1>
+
+	<center>
+    <a href="add.html" class="add">Add New Data</a><br/><br/>
+    <table width='90%' border=1>
+        <tr bgcolor='#CCCCCC'>
+			<td>ID</td>
+            <td>Email</td>
+            <td>Title</td>
+            <td>Body</td>
+            <td>Skills</td>
+            <td>Edit/Delete</td>
+        </tr>
+		</center>
+        <?php 
+        
+        while($res = mysqli_fetch_array($result)) {         
+            echo "<tr>";
+			echo "<td>".$res['id']."</td>";
+            echo "<td>".$res['email']."</td>";
+            echo "<td>".$res['title']."</td>";
+            echo "<td>".$res['body']."</td>"; 
+			echo "<td>".$res['skills']."</td>";  
+            echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";        
+        }
+        ?>
+		<br>
+	</div>
+    </table>
+</body>
+</html>
+
+
+<style>
+
+.question-box{
+    background-color: #99c0ff;
+    color: black;
+    margin: auto;
+    padding: 20px;
+    width: 65%;
+    border: 10px solid black;
+}
+
+.button {
+    width: auto;
+    padding: 10px;
+    font-weight: bold;
+    font-size: 100%;
+    background: #3366cc;
+    color: #fff;
+    border: 1px solid #3366cc;
+	float: right;
+}
+
+.add {
+    width: auto;
+    padding: 10px;
+    font-weight: bold;
+    font-size: 100%;
+    background: #5f94fc;
+    color: #fff;
+    border: 1px solid #3366cc;
+}
+
+</style>
